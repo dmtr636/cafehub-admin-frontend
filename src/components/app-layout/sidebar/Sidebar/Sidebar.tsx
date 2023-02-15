f5f2275f05a035cd226dbfb4443a753f5f222847
dmtr636 <dmtr636@gmail.com>
2023-02-16 00:23:42 +0300

    Sidebar

--- src/components/app-layout/sidebar/Sidebar/Sidebar.tsx
+++ src/components/app-layout/sidebar/Sidebar/Sidebar.tsx
@@ -1,12 +1,15 @@
 import React, {Fragment} from 'react';
 import styles from "./style.module.scss"
-import {sidebarRoutes} from "../../../../routes/sidebarRoutes";
+import {sidebarRoutes} from "@/routes/sidebarRoutes";
 import SidebarMenuItem from "../SidebarMenuItem/SidebarMenuItem";
 import SidebarNestedMenu from "../SidebarNestedMenu/SidebarNestedMenu";
+import {ReactComponent as Logo} from "@/assets/logo/logo.svg";
 
 const Sidebar = () => {
     return (
         <div className={styles.sidebar}>
+            <Logo/>
+            <input className={styles.search} placeholder={"Что искать?"} />
             <div className={styles.menu}>
                 {sidebarRoutes.map(route =>
                     <Fragment key={route.path}>
